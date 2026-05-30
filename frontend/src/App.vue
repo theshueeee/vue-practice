8c23a95051771d952fd1defe97baa1344ee5e9f3
theshueeee <theshueraj@gmail.com>
2026-05-30 16:33:32 +0800

    learning how to use refs, integrate logic inside vue app, adding tasks, editing values

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,59 @@
-<script setup></script>
+<script setup>
+import {ref} from 'vue';
+
+    const name = ref("Theshueraj");
+    const status = ref("PLANNED");
+    const tasks = ref(["task1", "task2", "task3"]);
+    const newTask = ref("");
+    const newName = ref("");
+
+    const ToggleStatus = () => {
+      if(status.value === "PLANNED"){
+        status.value = "IN PROGRESS";
+      } else if(status.value === "IN PROGRESS"){
+        status.value = "PLANNED";
+      }
+    };
+
+    const AddTask = () =>{
+      tasks.value.push(newTask.value);
+      newTask.value = "";
+    };
+
+    const ChangeName = () => {
+      name.value = newName.value;
+      newName.value = "";
+    };
+</script>
+
+
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div v-if="status === 'PLANNED'">{{ name}}</div>
+
+  <form @submit.prevent="AddTask">
+    <label for="newTask">Add Task</label>
+    <!-- As soon as you type anything onto the v-model here, it will be stored in newTask created above -->
+    <input type="text" v-model="newTask" />
+    <button type="submit">Add Task</button>
+  </form>
+
+
+  <form @submit.prevent="ChangeName">
+    <p for="newName">Change Name</p>
+    <input type = "text" v-model="newName">
+    <button type="submit">Change Name</button>
+  </form>
+
+
+  <p>Tasks</p>
+  <ul>
+    <li v-for="task in tasks":key="task">{{ task }}</li>
+  </ul>
+  <div>{{ status }}</div>
+  <button @click = "ToggleStatus">
+    Change Status
+  </button>
 </template>
 
-<style scoped></style>
+
---
+++ frontend/src/App2.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data () {
+    return {
+      name : "Shawshank Redemption",
+      status: "PLANNED",
+      tasks : ['task1', 'task2', 'task3'],
+    };
+  },
+
+  methods:{
+    ToggleStatus(){
+      if(this.status === "PLANNED"){
+        this.status = "IN PROGRESS";
+      } else if(this.status === "IN PROGRESS"){
+        this.status = "PLANNED";
+      }
+    }
+  }
+};
+</script>
+
+
+
+<template>
+  <div v-if="status === 'PLANNED'">{{ name}}</div>
+  <p>Tasks</p>
+  <ul>
+    <li v-for="task in tasks":key="task">{{ task }}</li>
+  </ul>
+  <div>{{ status }}</div>
+  <button @click = "ToggleStatus">
+    Change Status
+  </button>
+</template>
+
+

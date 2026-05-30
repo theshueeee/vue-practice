a78dc461dd780c722057ec174168c0fefcfc5177
theshueeee <theshueraj@gmail.com>
2026-05-30 18:39:43 +0800

    added delete task function, starting sample job project, added tailwindcss

--- frontend/src/App2.vue
+++ frontend/src/App2.vue
@@ -1,32 +1,73 @@
-<script>
-export default {
-  data () {
-    return {
-      name : "Shawshank Redemption",
-      status: "PLANNED",
-      tasks : ['task1', 'task2', 'task3'],
-    };
-  },
+<script setup>
+import {ref, onMounted} from 'vue';
 
-  methods:{
-    ToggleStatus(){
-      if(this.status === "PLANNED"){
-        this.status = "IN PROGRESS";
-      } else if(this.status === "IN PROGRESS"){
-        this.status = "PLANNED";
+    const name = ref("Theshueraj");
+    const status = ref("PLANNED");
+    const tasks = ref(["task1", "task2", "task3"]);
+    const newTask = ref("");
+    const newName = ref("");
+
+    const ToggleStatus = () => {
+      if(status.value === "PLANNED"){
+        status.value = "IN PROGRESS";
+      } else if(status.value === "IN PROGRESS"){
+        status.value = "PLANNED";
       }
+    };
+
+    const AddTask = () =>{
+      tasks.value.push(newTask.value);
+      newTask.value = "";
+    };
+
+    const ChangeName = () => {
+      name.value = newName.value;
+      newName.value = "";
+    };
+
+    const deleteTask=(index) => {
+      tasks.value.splice(index, 1);
     }
-  }
-};
+
+    onMounted(async () => {
+      try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+        const data = await response.json();
+        tasks.value = data.map((task) => task.title);
+      } catch (error){
+        console.error(error);
+      };
+    });
 </script>
 
 
 
 <template>
   <div v-if="status === 'PLANNED'">{{ name}}</div>
+
+  <form @submit.prevent="AddTask">
+    <label for="newTask">Add Task</label>
+    <!-- As soon as you type anything onto the v-model here, it will be stored in newTask created above -->
+    <input type="text" v-model="newTask" />
+    <button type="submit">Add Task</button>
+  </form>
+
+
+  <form @submit.prevent="ChangeName">
+    <p for="newName">Change Name</p>
+    <input type = "text" v-model="newName">
+    <button type="submit">Change Name</button>
+  </form>
+
+
   <p>Tasks</p>
   <ul>
-    <li v-for="task in tasks":key="task">{{ task }}</li>
+    <li v-for="(task,index) in tasks":key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">Delete</button>
+    </li>
   </ul>
   <div>{{ status }}</div>
   <button @click = "ToggleStatus">

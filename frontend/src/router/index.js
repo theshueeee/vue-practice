b0901c780010920fb16f5f21d1effe58d0ded07f
theshueeee <theshueraj@gmail.com>
2026-05-31 18:54:57 +0800

    added create job & delete job function

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import Homeview from '@/views/Homeview.vue';
 import Jobsview from '@/views/Jobsview.vue';
 import Notfoundview from '@/views/Notfoundview.vue';
 import Jobview from '@/views/Jobview.vue';
+import Addjobview from '@/views/Addjobview.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,10 @@ const router = createRouter({
         path: '/jobs/:id', // Match any path that hasn't been matched by other routes
         name: 'job',
         component: Jobview,
+    },{
+        path: '/jobs/add',
+        name: 'add-job',
+        component: Addjobview,
     },
   ],
 });
--- frontend/src/views/Jobview.vue
+++ frontend/src/views/Jobview.vue
@@ -1,11 +1,12 @@
 <script setup>
 import {PulseLoader} from 'vue-spinner';
 import axios from 'axios';
-
+import BackButton from '@/components/BackButton.vue';
 import { reactive, onMounted } from 'vue';
-import { useRoute, RouterLink } from 'vue-router';
+import { useRoute, RouterLink, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 
 const jobId = route.params.id;
 
@@ -24,9 +25,19 @@ onMounted(async () => {
     state.isLoading = false;
   }
 });
+
+const deleteJob = async () => {
+  try {
+    await axios.delete(`/api/jobs/${jobId}`);
+    router.push('/jobs');
+  } catch (error){
+    console.error('Error deleting job:', error);
+  }
+}
 </script>
 
 <template>
+  <BackButton />
   <section v-if="!state.isLoading" class="bg-green-50">
     <div class="container m-auto py-10 px-6">
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -94,7 +105,7 @@ onMounted(async () => {
               class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >Edit Job</RouterLink
             >
-            <button
+            <button @click="deleteJob"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
               Delete Job

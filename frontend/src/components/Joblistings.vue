9112249b4e3c346bb98554d4529fbc1a3f15d186
theshueeee <theshueraj@gmail.com>
2026-05-31 16:55:32 +0800

    fetched data from server port 5000 using axios

--- frontend/src/components/Joblistings.vue
+++ frontend/src/components/Joblistings.vue
@@ -1,8 +1,8 @@
 <script setup>
-import jobData from "@/jobs.json";
-import { ref, defineProps} from "vue";
+import { ref, defineProps, onMounted} from "vue";
 import {RouterLink} from "vue-router";
 import Joblisting from "./Joblisting.vue";
+import axios from "axios";
 
 defineProps({
     limit: {
@@ -14,7 +14,16 @@ defineProps({
     }
 });
 
-const jobs = ref(jobData.jobs);
+const jobs = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/jobs');
+    jobs.value = response.data;
+  } catch (error) {
+    console.error('Error fetching jobs:', error);
+  }
+});
 </script>
 
 <template>
